7961b4884798da97fb1019fe2352497c0c24d6d2
thoma <thomas.moreau1911@gmail.com>
2020-12-24 10:47:39 +0100

    Fin de la partie assertJ

--- src/main/java/testing/Calculator.java
+++ src/main/java/testing/Calculator.java
@@ -1,5 +1,8 @@
 package testing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
 
 	public int add(int a, int b) {
@@ -16,6 +19,15 @@ public class Calculator {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public Set<Integer> digitsSet(int number) {
+		Set<Integer> setIntegers = new HashSet<>();
+		String numberStr = String.valueOf(number);
+		for(char c : numberStr.toCharArray()) {
+			setIntegers.add(Integer.parseInt(String.valueOf(c)));
+		}
+		return setIntegers;
 	}
 
 }
